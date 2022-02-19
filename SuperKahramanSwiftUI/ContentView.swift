3e59c0774f2b4686d1eba18f820bafdbf6bf87e1
hsynaatik <93039124+hsynaatik@users.noreply.github.com>
2022-02-19 08:17:28 -0800

    Initial Commit

---
+++ SuperKahramanSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SuperKahramanSwiftUI
+//
+//  Created by Hüseyin Atık on 19.02.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SuperKahramanSwiftUI/SuperKahramanSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SuperKahramanSwiftUIApp.swift
+//  SuperKahramanSwiftUI
+//
+//  Created by Hüseyin Atık on 19.02.2022.
+//
+
+import SwiftUI
+
+@main
+struct SuperKahramanSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
